dc718fc7014882ed361ab6852edd5f201e7f4837
Moh. Naufal Azizi <naufalazizi.me@gmail.com>
2025-10-26 23:12:52 +0700

    feat:coba Assumptions

--- src/test/java/programmer/zaman/now/test/CalculatorTest.java
+++ src/test/java/programmer/zaman/now/test/CalculatorTest.java
@@ -1,31 +1,77 @@
 package programmer.zaman.now.test;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.function.Executable;
+import org.opentest4j.TestAbortedException;
+import programmer.zaman.now.test.generator.SimpleDisplayNameGenerator;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
 
+//@DisplayName("Test untuk Class Calculator ")
+@DisplayNameGeneration(value = SimpleDisplayNameGenerator.class)
 public class CalculatorTest {
 
     private Calculator calculator = new Calculator();
 
+    @BeforeAll
+    public static void beforeAll(){
+        System.out.println("Before All");
+    }
+
+    @AfterAll
+    public static void afterAll(){
+        System.out.println("After All");
+    }
+
+    @BeforeEach
+    public void setUp(){
+        System.out.println("BeforEach Test");
+    }
+
+    @AfterEach
+    public void tearDown(){
+        System.out.println("AfterEach Test");
+    }
+
     @Test
+//    @DisplayName("Test skenario sukses untuk method add(Integer, Integer)")
     public void testAddSuccess(){
         var result=calculator.add(10,10);
         Assertions.assertEquals(20,result);
     }
 
     @Test
+//    @DisplayName("Test skenario sukses untuk method divide(Integer, Integer)")
     public void testDivideSuccess(){
         var result = calculator.divide(100,10);
         Assertions.assertEquals(10,result);
     }
 
     @Test
+//    @DisplayName("Test skenario gagal untuk method divide(Integer, Integer)")
     public void testDivideFailed(){
         Assertions.assertThrows(IllegalArgumentException.class, ()->{
            calculator.divide(10,0);
         });
+    }
+
+    @Test
+    @Disabled
+    public void testCoomingSoon(){
+        //Cooming Soon Test
+    }
+
+    @Test
+    public void testAborted(){
+        var profile = System.getenv("PROFILE");
+        if(!"DEV".equals(profile)){
+            throw new TestAbortedException("Test dibatalkan karena bukan DEV");
+        }
+    }
+
+    @Test
+    public void testAssumptions(){
+        assumeTrue("DEV".equals(System.getenv("PROFILE")));
     }
 }
