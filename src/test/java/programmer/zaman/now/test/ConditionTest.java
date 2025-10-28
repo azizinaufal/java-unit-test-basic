1bd6173de177a0bc8113a2b72a2cb52afbd6e371
Moh. Naufal Azizi <naufalazizi.me@gmail.com>
2025-10-28 20:35:11 +0700

    Catatan belajar conditional test

--- src/test/java/programmer/zaman/now/test/ConditionTest.java
+++ src/test/java/programmer/zaman/now/test/ConditionTest.java
@@ -1,11 +1,19 @@
 package programmer.zaman.now.test;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledOnOs;
-import org.junit.jupiter.api.condition.EnabledOnOs;
-import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.api.condition.*;
+
+import java.util.Properties;
 
 public class ConditionTest {
+
+    @Test
+    @Disabled
+    void testSystemProperties(){
+        Properties properties = System.getProperties();
+        properties.forEach((key,value)-> System.out.println(key+ " "+ value));
+    }
 
     @Test
     @EnabledOnOs({OS.WINDOWS})
@@ -17,5 +25,60 @@ public class ConditionTest {
     @DisabledOnOs({OS.WINDOWS})
     public void testDisabledOnWindows(){
 
+    }
+
+    @Test
+    @EnabledOnJre({JRE.JAVA_8})
+    public void testEnabledOnJava8(){
+
+    }
+    @Test
+    @DisabledOnJre({JRE.JAVA_8})
+    public void testDisabledOnJava8(){
+
+    }
+
+    @Test
+    @EnabledForJreRange(min=JRE.JAVA_11, max=JRE.JAVA_15)
+    public void testEnabledOnJava11To15(){
+
+    }
+
+    @Test
+    @DisabledForJreRange(min=JRE.JAVA_11, max=JRE.JAVA_15)
+    public void testDisabledOnJava11To15(){
+
+    }
+
+    @Test
+    @EnabledIfSystemProperties({
+            @EnabledIfSystemProperty(named="java.vendor", matches = "Oracle Corporation")
+    })
+    void testEnabledOnJavaVendorOracle() {
+
+
+    }
+
+    @Test
+    @DisabledIfSystemProperties({
+            @DisabledIfSystemProperty(named="java.vendor", matches = "Oracle Corporation")
+    })
+    void testDisabledOnJavaVendorOracle() {
+
+
+    }
+
+    @Test
+    @EnabledIfEnvironmentVariables({
+            @EnabledIfEnvironmentVariable(named = "PROFILE", matches = "DEV")
+    })
+    void testEnabledOnProfileDev() {
+    }
+
+    @Test
+    @DisabledIfEnvironmentVariables({
+            @DisabledIfEnvironmentVariable(named = "PROFILE", matches = "DEV")
+    })
+    void testDisabledOnProfileDev() {
     }
 }
